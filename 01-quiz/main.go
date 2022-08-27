1f98a84803bfdf00df6fb0f9fc5e0fe00a729f29
sumeetmathpati <sumeet.mathpati@gmail.com>
2022-08-28 01:34:05 +0530

    Added Question type.

--- 01-quiz/main.go
+++ 01-quiz/main.go
@@ -1,5 +1,15 @@
 package main
 
+type Question struct {
+	Data          string
+	Score         int
+	OptionA       string
+	OptionB       string
+	OptionC       string
+	OptionD       string
+	CorrectOption string
+}
+
 func main() {
 
 }
