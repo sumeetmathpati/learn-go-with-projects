ba41b052f07462190b7a7a0d51220c59f00ea378
sumeetmathpati <sumeet.mathpati@gmail.com>
2022-08-28 02:12:11 +0530

    Added questions.

--- 01-quiz/main.go
+++ 01-quiz/main.go
@@ -12,4 +12,13 @@ type Question struct {
 
 func main() {
 
+	// Question DB
+	questions := []Question{
+		Question{"Which Linux distro uses names from movie Toy story for it's releases?", 2, "Ubuntu", "Debian", "Fedora", "Arch", "b"},
+		Question{"Where were The Lord of the Rings movies filmed?", 2, "Ireland", "Iceland", "New Zealand", "Australia", "c"},
+		Question{"Which country does Forrest Gump travel to as part of the All-American Ping-Pong Team?", 2, "Vietnam", "China", "Sweden", "France", "b"},
+		Question{"What are the words the orcs of Mordor interrogated out of Gollum?", 1, "Shire, Baggins", "Gandalf, Hobbiton", " Ring, Baggins", " Shire, Frodo", "a"},
+		Question{"What's the name of the dog guarding the Sorcerer’s Stone?", 2, "Padfoot", "Grim", "Fluffy", "Fang", "c"},
+	}
+
 }
